7dba746ceabf5dd9e8022cf0e87dba09f00bba4b
Milad M. Alemansour <milad.alemansour@gmail.com>
2019-08-17 19:58:20 +0430

    Spring Security in-memory sample Controller Login error fixed. LoginPage variable for login error fixed,

--- spring-security-in-memory-sample/src/main/java/com/miladalemansour/springsecuritysample/controller/LoginController.java
+++ spring-security-in-memory-sample/src/main/java/com/miladalemansour/springsecuritysample/controller/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
     @RequestMapping("/login")
     public String login(Model model) {
-
+        model.addAttribute("loginError", false);
         return "loginPage";
     }
 
